25aed33f10e4260781419efd55f89f8c5fc6d599
Prisca Ogu <chidimma.p.ogu@gmail.com>
2024-05-28 13:00:20 -0700

    function updatedd

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import pickle
 import pandas as pd
-import os
 import streamlit as st
-import sklearn
 from sklearn.ensemble import RandomForestClassifier
 
 
-# Function to load the model
 def load_model():
-    with open("cr_model.pkt", "rb") as file:
-        rf = pickle.load(file)
+    rf = pickle.load(open("cr_model.pkt", "rb"))
     return rf
+
+
+# Streamlit app
+st.header("Credit Default Prediction App")
+st.subheader("Input your data")
+#data_input = st.data_input("Enter your data")
+
+# Create input fields for each required feature
+person_default_input = st.selectbox("Previous Default", ['No', 'Yes'])
+home_ownership_input = st.selectbox("Home Ownership", ["RENT", "OWN", "MORTGAGE", "OTHER"])
+person_income = st.number_input("Person Income", min_value=0)
+loan_amnt = st.number_input("Loan Amount", min_value=0)
+loan_int_rate = st.number_input("Loan Interest Rate", min_value=0.0, max_value=20.0, format="%.2f")
+loan_percent_income = st.number_input("Loan Percent Income", min_value=0.0, max_value=1.0, format="%.2f")
 
 # Function to convert home ownership to numerical value
 def home_ownership(x):
@@ -29,6 +39,24 @@ def person_default(x):
     return 1 if x == 'Yes' else 0
 
 
+#Ordinal value arrangement 
+home_ownership_ord = home_ownership(home_ownership_input)
+person_default_ord = person_default(person_default_input)
+
+
+# Function to prepare data input
+def prepare_data(person_default_ord, home_ownership_ord, person_income, loan_amnt, loan_int_rate, loan_percent_income):
+    data = {
+        'person_default': person_default_ord,
+        'home_ownership': home_ownership_ord,
+        'person_income': person_income,
+        'loan_amnt': loan_amnt,
+        'loan_int_rate': loan_int_rate,
+        'loan_percent_income': loan_percent_income
+    }
+    return data
+
+
 # Function to preprocess input data and make a prediction
 def creditRisk_prediction(data):
     rf = load_model()
@@ -42,34 +70,6 @@ def creditRisk_prediction(data):
     except Exception as e:
         st.error(f"Error during prediction: {e}")
         return "Prediction failed."
-
-# Function to prepare data input
-def prepare_data(person_default_ord, home_ownership_ord, person_income, loan_amnt, loan_int_rate, loan_percent_income):
-    data = {
-        'person_default': person_default(person_default_ord),
-        'home_ownership': home_ownership(home_ownership_ord),
-        'person_income': person_income,
-        'loan_amnt': loan_amnt,
-        'loan_int_rate': loan_int_rate,
-        'loan_percent_income': loan_percent_income
-    }
-    return data
-
-# Streamlit app
-st.header("Credit Default Prediction App")
-st.subheader("Input your data")
-
-# Create input fields for each required feature
-person_default_input = st.selectbox("Previous Default", ['No', 'Yes'])
-home_ownership_input = st.selectbox("Home Ownership", ["RENT", "OWN", "MORTGAGE", "OTHER"])
-person_income = st.number_input("Person Income", min_value=0)
-loan_amnt = st.number_input("Loan Amount", min_value=0)
-loan_int_rate = st.number_input("Loan Interest Rate", min_value=0.0, max_value=20.0, format="%.2f")
-loan_percent_income = st.number_input("Loan Percent Income", min_value=0.0, max_value=1.0, format="%.2f")
-
-#Ordinal value arrangement 
-home_ownership_ord = home_ownership(home_ownership_input)
-person_default_ord = person_default(person_default_input)
 
 # Prepare the data input
 data_input = prepare_data(person_default_ord, home_ownership_ord, person_income, loan_amnt, loan_int_rate, loan_percent_income)
